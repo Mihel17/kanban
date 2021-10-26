c8d0f0d6db503473314febb02cdd013fb04b6905
Miguel <fedosieiev.mihel@gmail.com>
2021-10-26 17:55:55 +0300

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,98 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: './js/script.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'core')
+module.exports = (evt, argv) => {
+  const isProd = argv.mode === 'production'
+  const isDev = !isProd
+
+  console.log(`isProd: ${isProd}`)
+  console.log(`isDev: ${isDev}`)
+
+  const filename = (ext) =>
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+  const plugins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'css'),
+            to: path.resolve(__dirname, 'dist', 'css')
+          },
+          {
+            from: path.resolve(__dirname, 'src', 'fonts'),
+            to: path.resolve(__dirname, 'dist', 'fonts')
+          },
+          {
+            from: path.resolve(__dirname, 'src', 'img'),
+            to: path.resolve(__dirname, 'dist', 'img')
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css'),
+      }),
+
+    ]
+    if (isDev) {
+      base.push(new ESLintPlugin())
     }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: path.resolve(__dirname, 'src', 'css'), to: path.resolve(__dirname, 'dist', 'css') },
-        { from: path.resolve(__dirname, 'src', 'fonts'), to: path.resolve(__dirname, 'dist', 'fonts') },
-        { from: path.resolve(__dirname, 'src', 'img'), to: path.resolve(__dirname, 'dist', 'img') },
+
+    return base
+  }
+
+  return {
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: ['@babel/polyfill', './js/script.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true
+    },
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'core')
+      }
+    },
+    plugins: plugins(),
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      watchFiles: './',
+    },
+    devtool: isDev ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
-    }),
-  ]
+    }
+  }
 }
